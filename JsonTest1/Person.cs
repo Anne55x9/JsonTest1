6d5fb4d42f7ec2f59580606ffaee04344f904964
anne55x9 <anne55x9@edu.easj.dk>
2016-11-22 13:04:40 +0100

    jSonTestWork

---
+++ JsonTest1/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTest1
+{
+    class Person
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        private int tlf;
+
+        public int Tlf
+        {
+            get { return tlf; }
+            set { tlf = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Person()
+        {
+
+        }
+
+    }
+}
--- JsonTest1/Program.cs
+++ JsonTest1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JsonTest1
 {
@@ -10,6 +11,17 @@ namespace JsonTest1
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person()
+            {
+                Name = "Anne",
+                Tlf= 26573928,
+                Email = "a@yahoo.com" 
+        };
+
+            string jsonText = JsonConvert.SerializeObject(person1);
+            Console.WriteLine(jsonText);
+
+            Console.ReadLine();
         }
     }
 }
